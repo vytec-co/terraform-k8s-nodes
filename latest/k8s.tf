157dada68bdea525b58ab7e2a12571a42d968252
vsm <devops.july2.2017@gmail.com>
2020-09-09 09:57:38 +0530

    Update k8s.tf

--- latest/k8s.tf
+++ latest/k8s.tf
@@ -1,3 +1,5 @@
+variable "ssh_key_name" {default = "ohio"}
+variable "aws_region_name" { default = "us-east-2" }
 provider "aws" {
   region = "us-east-2"
 }
@@ -18,11 +20,65 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "k8s-master" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  count         = "1"
+  pc_security_group_ids = [aws_security_group.k8s_sg.id]
+  key_name = var.ssh_key_name
+  tags = {
+    Name = "k8s-master"
+  }
+}
+
+resource "aws_instance" "k8s-worker" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   count         = "2"
+  pc_security_group_ids = [aws_security_group.k8s_sg.id]
+  key_name = var.ssh_key_name
   tags = {
-    Name = "HelloWorld"
+    Name = "k8s-worker"
   }
+}
+resource "aws_security_group" "k8s_sg" {
+
+}
+resource "aws_security_group_rule" "allow_all_egress" {
+  type            = "egress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "all"
+  cidr_blocks     = ["0.0.0.0/0"]
+  description     = "Outbound access to ANY"
+
+  security_group_id = aws_security_group.k8s_sg.id
+}
+resource "aws_security_group_rule" "allow_all_myip" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "all"
+  cidr_blocks     = ["${data.external.myipaddr.result["ip"]}/32"]
+  description     = "Management Ports for K8s Cluster"
+
+  security_group_id = "${aws_security_group.k8s_sg.id}"
+}
+
+resource "aws_security_group_rule" "allow_SG_any" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "all"
+  self            = true
+  description     = "Any from SG for K8s Cluster"
+
+  security_group_id = aws_security_group.k8s_sg.id
+}
+
+output "master_ip" {
+  value = aws_spot_instance_request.k8s-master.public_ip
+}
+output "worker_ips" {
+  value = aws_spot_instance_request.k8s-worker.*.public_ip
 }
